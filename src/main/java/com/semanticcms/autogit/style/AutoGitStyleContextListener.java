102afa06913028dbf554d9535b5d18d4145558dc
AO Industries, Inc. <support@aoindustries.com>
2016-08-26 09:59:21 -0500

    Moving automatic Git to separate packages.

---
+++ src/main/java/com/semanticcms/autogit/style/AutoGitStyleContextListener.java
@@ -0,0 +1,44 @@
+/*
+ * semanticcms-autogit-style - Default style for SemanticCMS automatic Git.
+ * Copyright (C) 2016  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of semanticcms-autogit-style.
+ *
+ * semanticcms-autogit-style is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * semanticcms-autogit-style is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with semanticcms-autogit-style.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.semanticcms.autogit.style;
+
+import com.semanticcms.core.servlet.SemanticCMS;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener("Registers the styles for AutoGit in SemanticCMS.")
+public class AutoGitStyleContextListener implements ServletContextListener {
+
+	@Override
+	public void contextInitialized(ServletContextEvent event) {
+		SemanticCMS semanticCMS = SemanticCMS.getInstance(event.getServletContext());
+		// Add our CSS file
+		semanticCMS.addCssLink("/semanticcms-autogit-style/styles.css");
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent event) {
+		// Do nothing
+	}
+}
