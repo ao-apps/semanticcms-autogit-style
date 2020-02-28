2ea7d0bef2fb877128b5219fc11f5ea9a469aaf2
AO Industries, Inc <support@aoindustries.com>
2020-02-28 11:17:41 -0600

    Renamed *.css and *.js to have more distinct filenames.
    The developer tools in Firefox simply show the filename without path.
    Thus, we were seeing "scripts.js", "style.css", "style.css", …
    This should make it easier to debug styles and scripts.

--- src/main/java/com/semanticcms/autogit/style/Initializer.java
+++ src/main/java/com/semanticcms/autogit/style/Initializer.java
@@ -34,7 +34,7 @@ public class Initializer implements ServletContextListener {
 	public void contextInitialized(ServletContextEvent event) {
 		SemanticCMS semanticCMS = SemanticCMS.getInstance(event.getServletContext());
 		// Add our CSS file
-		semanticCMS.addCssLink("/semanticcms-autogit-style/styles.css");
+		semanticCMS.addCssLink("/semanticcms-autogit-style/semanticcms-autogit.css");
 	}
 
 	@Override
