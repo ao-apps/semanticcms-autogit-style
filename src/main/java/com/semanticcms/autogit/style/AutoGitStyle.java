66c7b20ae82631ca96574de15c740bc020997571
AO Industries, Inc. <support@aoindustries.com>
2020-03-03 23:20:14 -0600

    Registered style in ao-web-resources

--- src/main/java/com/semanticcms/autogit/style/AutoGitStyle.java
+++ src/main/java/com/semanticcms/autogit/style/AutoGitStyle.java
@@ -22,19 +22,21 @@
  */
 package com.semanticcms.autogit.style;
 
-import com.semanticcms.core.servlet.SemanticCMS;
+import com.aoindustries.web.resources.registry.Style;
+import com.aoindustries.web.resources.servlet.RegistryEE;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
-@WebListener("Registers the styles for AutoGit in SemanticCMS.")
-public class Initializer implements ServletContextListener {
+@WebListener("Registers the styles for AutoGit in RegistryEE.")
+public class AutoGitStyle implements ServletContextListener {
+
+	public static final Style SEMANTICCMS_AUTOGIT = new Style("/semanticcms-autogit-style/semanticcms-autogit.css");
 
 	@Override
 	public void contextInitialized(ServletContextEvent event) {
-		SemanticCMS semanticCMS = SemanticCMS.getInstance(event.getServletContext());
 		// Add our CSS file
-		semanticCMS.addCssLink("/semanticcms-autogit-style/semanticcms-autogit.css");
+		RegistryEE.get(event.getServletContext()).global.styles.add(SEMANTICCMS_AUTOGIT);
 	}
 
 	@Override
