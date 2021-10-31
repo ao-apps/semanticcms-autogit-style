769c470e7b421193b2e17c40c8f9886fb6a9e62a
AO Industries, Inc <support@aoindustries.com>
2021-10-30 23:50:34 -0500

    Changed links to https for https://www.gnu.org/licenses/

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -18,7 +18,7 @@
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with semanticcms-autogit-style.  If not, see <http://www.gnu.org/licenses/>.
+ * along with semanticcms-autogit-style.  If not, see <https://www.gnu.org/licenses/>.
  */
 module com.semanticcms.autogit.style {
 	exports com.semanticcms.autogit.style;
