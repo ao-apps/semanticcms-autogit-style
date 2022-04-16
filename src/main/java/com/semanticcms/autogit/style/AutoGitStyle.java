ac69da271ef16c0852406c67dcb7d8f9e65a5545
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:02:47 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/semanticcms/autogit/style/AutoGitStyle.java
+++ src/main/java/com/semanticcms/autogit/style/AutoGitStyle.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-autogit-style - Default style for SemanticCMS automatic Git.
- * Copyright (C) 2016, 2017, 2020, 2021  AO Industries, Inc.
+ * Copyright (C) 2016, 2017, 2020, 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with semanticcms-autogit-style.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.semanticcms.autogit.style;
 
 import com.aoapps.web.resources.registry.Group;
